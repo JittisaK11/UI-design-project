7ab0432e9f914884dd41d2f97df907e9bcf3cf0b
jamesyu13 <82225616+jamesyu13@users.noreply.github.com>
2024-04-26 16:35:18 -0400

    fixed server

--- server.py
+++ server.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, session, request, jsonify
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
+# Initialize an empty list to store scores
+scores = []
 
 # ROUTES
 @app.route('/')
 def home():
+    session['score'] = 0
     return render_template('home.html')
 
 # Beat and Tempo page route
@@ -13,10 +16,22 @@ def home():
 def beat_and_tempo():
     return render_template('beat-and-tempo.html')
 
+@app.route('/common-tempo')
+def common():
+   return render_template('common-tempo.html')
+
 # Duration and Symbols page route
 @app.route('/duration-and-symbols')
 def duration_and_symbols():
     return render_template('duration-and-symbols.html')
+
+@app.route('/duration-and-symbols-pg2')
+def duration_and_symbols2():
+    return render_template('duration-and-symbols-pg2.html')
+
+@app.route('/duration-and-symbols-pg3')
+def duration_and_symbols3():
+    return render_template('duration-and-symbols-pg3.html')
 
 # Subdividing page route
 @app.route('/subdividing')
@@ -28,23 +43,41 @@ def subdividing_pg2():
     return render_template('subdividing-pg2.html')
 
 # Quiz page route
-@app.route('/quiz')
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    # Set initial score to 0 if it doesn't exist in session
-    if 'score' not in session:
-        session['score'] = 0
     return render_template('index.html')
+    
+@app.route('/score', methods=['GET'])
+def get_score():
+    if 'score' in session:
+        return jsonify({'score': session['score']})
+    else:
+        return jsonify({'score': 0})
+    
+@app.route('/increment-score', methods=['POST'])
+def increment_score():
+    if 'score' in session:
+        session['score'] += 1
+    else:
+        session['score'] = 1
+    return jsonify({'success': True})
+
+@app.route('/increment-score2', methods=['POST'])
+def increment_score2():
+    if 'score' in session:
+        session['score'] += 3
+    else:
+        session['score'] = 1
+    return jsonify({'success': True})
 
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
+    # Set initial score to 0 if it doesn't exist in session
+    if 'score' not in session:
+        session['score'] = 0
     return render_template('quiz.html')
 
-@app.route('/common-tempo')
-def common():
-    return render_template('common-tempo.html')
-
-
+# Dynamic route for specific quiz pages like quiz1, quiz2, etc.
 @app.route('/quiz<int:quiz_number>', methods=['GET', 'POST'])
 def specific_quiz(quiz_number):
     # Construct the template filename based on quiz_number
