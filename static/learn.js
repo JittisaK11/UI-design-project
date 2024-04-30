338576b6eedb38e1bf3e3f36a0ef99565dcc1d33
Mori Liu <kl3186@barnard.edu>
2024-04-30 15:33:03 -0400

    on page display

--- static/learn.js
+++ static/learn.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 '<strong>Tempo</strong> refers to the speed or pace of a given piece of music, essentially determining how fast or slow the beat is.',
                 'It is typically measured in <strong>beats per minute (BPM)</strong>, with a higher BPM indicating a faster tempo.',
                 'The tempo sets the overall <strong>energy level</strong> of the music and can significantly affect the mood and feel of a piece.',
-                '<div class="instr">Click here to learn about the common tempos:</div> <button class="btn-newpage" > <a href="' + tempoURL + '"  style="color: white" >Common Tempo</a></button> </div>'
+                '<button id="commonTemposButton">Learn about Common Tempos</button>'
             ]);
         });
         button3.addEventListener('click', () => displayText([
@@ -32,4 +32,34 @@ function displayText(points) {
     const displayArea = document.getElementById('display-area');
     const formattedText = '<ul>' + points.map(point => `<li>${point}</li>`).join('') + '</ul>';
     displayArea.innerHTML = formattedText;
+
+    const commonTemposButton = document.getElementById('commonTemposButton');
+    if (commonTemposButton) {
+        commonTemposButton.addEventListener('click', displayCommonTempos);
+    }
+}
+
+function displayCommonTempos() {
+    const htmlContent = `
+        <div class="container">
+            <h1>Common Tempos</h1>
+            <p>There are standard tempo terms that are used in music. They let us know the general pace of the piece of music.
+            Tempo terms are typically conveyed through Italian words, and can be specified by a certain beats per minute (BPM) range.
+            Learn some common ones here:</p>
+            <div class="grid-container">
+                <div class="grid-item">Adagio
+                    <audio controls src="/media/cc0-audio/t-rex-roar.mp3"></audio>
+                </div>
+                <div class="grid-item">Andante
+                    <audio controls src="/media/cc0-audio/t-rex-roar.mp3"></audio>
+                </div>
+                <div class="grid-item">Allegro
+                    <audio controls src="/media/cc0-audio/t-rex-roar.mp3"></audio>
+                </div>
+            </div>
+            <a href="#" onclick="history.back();">Take Me Back</a>
+        </div>
+    `;
+    const displayArea = document.getElementById('display-area');
+    displayArea.innerHTML += htmlContent;
 }
